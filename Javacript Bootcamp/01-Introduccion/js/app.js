24d219f275bdeabe8e11697ddfa48da9e2d809ab
yorlysoro <yorlysoro@gmail.com>
2025-07-26 21:53:30 -0400

    [ADD] agregado primeros pasos con javascript

---
+++ Javacript Bootcamp/01-Introduccion/js/app.js
@@ -0,0 +1,7 @@
+//alert("¡Hola, JavaScript Moderno!");
+//const nombre = prompt("¿Cuál es tu nombre?");
+//alert(`¡Hola, ${nombre}! Bienvenido al curso de JavaScript Moderno.`);
+//document.querySelector(".contenido").innerHTML = `${nombre}, este es el curso de JavaScript Moderno. ¡Disfrútalo!`;
+
+// Variables y constantes
+const producto = 'Monitor 20 pulgadas';
