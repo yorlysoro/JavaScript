f9c7bcfe1b630bf340fa38565f0dc162e1e5b4b9
yorlysoro <yorlysoro@gmail.com>
2025-07-29 22:23:54 -0400

    agregada nuevas funciones de numeros

--- Javacript Bootcamp/04-Numeros/js/06-app.js
+++ Javacript Bootcamp/04-Numeros/js/06-app.js
@@ -4,3 +4,7 @@ console.log(Number(numero)); // 5
 
 console.log(parseInt(numero)); // 5
 console.log(parseFloat(numero)); // 5
+
+// isInteger
+console.log(Number.isInteger(5)); // true
+console.log(Number.isInteger(5.5)); // false
