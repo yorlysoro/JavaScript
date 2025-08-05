b3ff3f955bb83d40ab5040cd8430cca503e3a831
yorlysoro <yorlysoro@gmail.com>
2025-08-04 22:27:50 -0400

    [ADD] iniciando con javascript moderno

---
+++ React Bootcamp/02-intro-javacript/src/index.js
@@ -0,0 +1,14 @@
+const nombre = "Fernando";
+let apellido = "Herrera";
+
+console.log(`Hola, ${nombre} ${apellido}`);
+
+let valorDado = 5;
+console.log(valorDado);
+
+if (true) {
+    let valorDado = 4;
+    console.log(valorDado);
+}
+
+console.log(valorDado);
