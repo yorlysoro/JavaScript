a4a33deb5a81ed596bbfd627a3c6e4d968b7859d
yorlys <yorlysoro@gmail.com>
2025-12-11 21:26:24 -0400

    add added new alerts in fields of html form

--- Javacript Bootcamp/16-PROYECTO-EnviarEmail/js/app.js
+++ Javacript Bootcamp/16-PROYECTO-EnviarEmail/js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     // Asignar eventos
     inputEmail.addEventListener('blur', validar);
@@ -13,8 +14,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function validar(e){
         if(e.target.value.trim() === ''){
-            console.log('El campo ' + e.target.id + ' es obligatorio');
+            mostrarAlerta('El campo ' + e.target.id + ' es obligatorio', e.target.parentElement);
+        } else {
+            console.log('El campo ' + e.target.id + ' es correcto');
         }
+    }
+
+    function mostrarAlerta(mensaje, referencia){
+        // Si ya existe una alerta, no crear otra
+        const alertaPrevia = referencia.querySelector('.bg-red-600');
+        if(alertaPrevia){
+            return;
+        }
+
+        // Crear la alerta
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-5');
+        
+        // Insertar en el formulario
+        referencia.appendChild(error);
     }
 });
 
